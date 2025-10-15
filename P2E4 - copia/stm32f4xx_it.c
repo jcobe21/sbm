95994d37684d19395c99462d5f27d316efbbc64c
susanatellez <susana.tellez@alumnos.upm.es>
2025-10-15 12:52:57 +0200

    P2E4
    funciona

---
+++ P2E4 - copia/stm32f4xx_it.c
@@ -0,0 +1,255 @@
+/**
+  ******************************************************************************
+  * @file    Templates/Src/stm32f4xx_it.c 
+  * @author  MCD Application Team
+  * @brief   Main Interrupt Service Routines.
+  *          This file provides template for all exceptions handler and 
+  *          peripherals interrupt service routine.
+  *
+  * @note    modified by ARM
+  *          The modifications allow to use this file as User Code Template
+  *          within the Device Family Pack.
+  ******************************************************************************
+  * @attention
+  *
+  * <h2><center>&copy; COPYRIGHT(c) 2017 STMicroelectronics</center></h2>
+  *
+  * Redistribution and use in source and binary forms, with or without modification,
+  * are permitted provided that the following conditions are met:
+  *   1. Redistributions of source code must retain the above copyright notice,
+  *      this list of conditions and the following disclaimer.
+  *   2. Redistributions in binary form must reproduce the above copyright notice,
+  *      this list of conditions and the following disclaimer in the documentation
+  *      and/or other materials provided with the distribution.
+  *   3. Neither the name of STMicroelectronics nor the names of its contributors
+  *      may be used to endorse or promote products derived from this software
+  *      without specific prior written permission.
+  *
+  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+  * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+  * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+  * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+  * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+  *
+  ******************************************************************************
+  */
+
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+#include "stm32f4xx_it.h"
+
+#ifdef _RTE_
+#include "RTE_Components.h"             /* Component selection */
+#endif
+
+/** @addtogroup STM32F4xx_HAL_Examples
+  * @{
+  */
+
+/** @addtogroup Templates
+  * @{
+  */
+
+/* Private typedef -----------------------------------------------------------*/
+/* Private define ------------------------------------------------------------*/
+/* Private macro -------------------------------------------------------------*/
+/* Private variables ---------------------------------------------------------*/
+
+extern TIM_HandleTypeDef tim2;                                                  //
+extern TIM_HandleTypeDef tim3;                                                  //
+
+uint16_t captures[2];
+uint8_t captureDone = 0;
+uint16_t diffCapture = 0;
+float frequency;
+
+/* Private function prototypes -----------------------------------------------*/
+
+void EXTI15_10_IRQHandler(void);                                                //
+void TIM3_IRQHandler(void);                                                     //
+
+/* Private functions ---------------------------------------------------------*/
+
+void EXTI15_10_IRQHandler(void) {                                               //INTERRUPCION BOTÓN
+  HAL_GPIO_EXTI_IRQHandler(GPIO_PIN_13);                                        //External Interrupt = EXTI..
+}                                                                               //
+
+void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin) {                                //lo que quieres que haga cuando aprietes el botón
+
+  HAL_TIM_OC_Stop(&tim2, TIM_CHANNEL_4);                                        //Para cambiar parámetros de timers, debemos pararlo, cambiar y runear
+  
+  if (tim2.Init.Period == 49) {                                                 //100 000 / 50 = 2kHz (arriba=abajo) = 1kHz (arriba + abajo)
+    tim2.Init.Period = 19;                                                      //
+  } else {                                                                      //100 000 / 20 = 5kHz (arriba=abajo) = 2.500Hz (arriba + abajo)
+    tim2.Init.Period = 49;                                                      //
+  }  //tim2.Init.Period = tim2.Init.Period == 49 ? 19 : 49;
+
+  HAL_TIM_OC_Init(&tim2);                                                       //
+  HAL_TIM_OC_Start(&tim2, TIM_CHANNEL_4);                                       //
+
+}
+
+void TIM3_IRQHandler(void)
+{
+  HAL_TIM_IRQHandler(&tim3);                                                    //
+}
+
+void HAL_TIM_IC_CaptureCallback(TIM_HandleTypeDef* htim)
+{
+  if(htim == &tim3){                                                            //Comprueba si la interrupción vino del TIM3
+    if(captureDone!=2){                                                         //si no hay ninguna captura, entonces
+      captures[captureDone] = HAL_TIM_ReadCapturedValue(&tim3, TIM_CHANNEL_1);  //la "haces" y la guardas
+      captureDone++;                                                            // e incrementas la cuenta
+    }
+  }
+  
+  if(captureDone == 2){                                                         //Si hay capturas
+    if(captures[1] >= captures[0])                                              //y el tiempo de la segunda es mayor que la primera
+      diffCapture = captures[1] - captures[0];                                  //sacamos la diferencia de captura de forma normal
+    else                                                                        //si no es el caso
+      diffCapture = (tim3.Init.Period + 1 - captures[0]) + captures[1];         //
+
+    frequency = (float) 84000000 / ((tim3.Init.Prescaler + 1) * (diffCapture)); //calculo de la frecuencia de la señal
+    captureDone = 0;
+    }
+}
+
+/******************************************************************************/
+/*            Cortex-M4 Processor Exceptions Handlers                         */
+/******************************************************************************/
+
+/**
+  * @brief   This function handles NMI exception.
+  * @param  None
+  * @retval None
+  */
+void NMI_Handler(void)
+{
+}
+
+/**
+  * @brief  This function handles Hard Fault exception.
+  * @param  None
+  * @retval None
+  */
+void HardFault_Handler(void)
+{
+  /* Go to infinite loop when Hard Fault exception occurs */
+  while (1)
+  {
+  }
+}
+
+/**
+  * @brief  This function handles Memory Manage exception.
+  * @param  None
+  * @retval None
+  */
+void MemManage_Handler(void)
+{
+  /* Go to infinite loop when Memory Manage exception occurs */
+  while (1)
+  {
+  }
+}
+
+/**
+  * @brief  This function handles Bus Fault exception.
+  * @param  None
+  * @retval None
+  */
+void BusFault_Handler(void)
+{
+  /* Go to infinite loop when Bus Fault exception occurs */
+  while (1)
+  {
+  }
+}
+
+/**
+  * @brief  This function handles Usage Fault exception.
+  * @param  None
+  * @retval None
+  */
+void UsageFault_Handler(void)
+{
+  /* Go to infinite loop when Usage Fault exception occurs */
+  while (1)
+  {
+  }
+}
+
+/**
+  * @brief  This function handles SVCall exception.
+  * @param  None
+  * @retval None
+  */
+#ifndef RTE_CMSIS_RTOS2_RTX5
+void SVC_Handler(void)
+{
+}
+#endif
+
+/**
+  * @brief  This function handles Debug Monitor exception.
+  * @param  None
+  * @retval None
+  */
+void DebugMon_Handler(void)
+{
+}
+
+/**
+  * @brief  This function handles PendSVC exception.
+  * @param  None
+  * @retval None
+  */
+#ifndef RTE_CMSIS_RTOS2_RTX5
+void PendSV_Handler(void)
+{
+}
+#endif
+
+/**
+  * @brief  This function handles SysTick Handler.
+  * @param  None
+  * @retval None
+  */
+#ifndef RTE_CMSIS_RTOS2_RTX5
+void SysTick_Handler(void)
+{
+  HAL_IncTick();
+}
+#endif
+
+/******************************************************************************/
+/*                 STM32F4xx Peripherals Interrupt Handlers                   */
+/*  Add here the Interrupt Handler for the used peripheral(s) (PPP), for the  */
+/*  available peripheral interrupt handler's name please refer to the startup */
+/*  file (startup_stm32f4xx.s).                                               */
+/******************************************************************************/
+
+/**
+  * @brief  This function handles PPP interrupt request.
+  * @param  None
+  * @retval None
+  */
+/*void PPP_IRQHandler(void)
+{
+}*/
+
+
+/**
+  * @}
+  */ 
+
+/**
+  * @}
+  */
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
